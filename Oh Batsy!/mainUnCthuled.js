3ef6ca5dc15baa8af0fe921dd5c1655042e15f01
Jorge Rodriguez <jorguero47@gmail.com>
2019-10-17 22:37:18 +0200

    Huellas y limites del mapa completed

--- Oh Batsy!/mainUnCthuled.js
+++ Oh Batsy!/mainUnCthuled.js
@@ -1,5 +1,5 @@
 let arrayMapa = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0],
     [0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0],
@@ -12,81 +12,81 @@ let arrayMapa = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0],
     [0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ];
 
-let posicionPersonajeX = 8; //ARRIBA(-1) ABAJO(+1)
+let posicionPersonajeX = 9; //ARRIBA(-1) ABAJO(+1)
 let posicionPersonajeY = 0; //IZQUIERDA(-1) DERECHA(+1)
+
+let posicionHuellaX = posicionPersonajeX - 1; //ARRIBA(-1) ABAJO(+1)
+let posicionHuellaY = posicionPersonajeY - 1; //IZQUIERDA(-1) DERECHA(+1)
 
 
 window.onload = function() {
 
-establecerMovimiento();
+    establecerMovimiento();
 
 }
+
 function establecerMovimiento(img) {
-    
+
 
     for (let i = 0; i < 14; i++) {
 
         for (let j = 0; j < 21; j++) {
 
             var newDiv = document.createElement("div");
+            if (arrayMapa[i][j] == 2) {
 
-            if (i == 0 & j != 8) {
-                newDiv.classList.add("techo");
+                newDiv.classList.add("puerta");
 
             }
 
 
+
             if (i == posicionPersonajeY & j == posicionPersonajeX) {
-               /* if (img == "batmanIzquierda") {
-                    newDiv.classList.add("personajeIzquierda");
-
-                }else{
-                    newDiv.classList.add("personaje");
-
-                }*/
-
-                newDiv.classList.add("puerta");
 
                 switch (img) {
 
                     case "batmanIzquierda":
                         newDiv.classList.add("personajeIzquierda");
-                    break;
-                
+                        break;
+
                     case "batmanDerecha":
                         newDiv.classList.add("personajeDerecha");
-                    break;
-                
+                        break;
+
                     case "batmanAbajo":
                         newDiv.classList.add("personajeAbajo");
-                    break;
-                
+                        break;
+
                     case "batmanArriba":
                         newDiv.classList.add("personajeArriba");
-                    break;
-                
+                        break;
+
                 }
             }
 
-            if (i == 13 & j == 15) {
-                newDiv.classList.add("villano");
-            }
+            /* if (i == 13 & j == 15) {
+                 newDiv.classList.add("villano");
+             }*/
 
             if (arrayMapa[i][j] == 0) {
 
                 newDiv.classList.add("camino");
 
-            } else {
+            } else if (arrayMapa[i][j] == 1) {
                 newDiv.classList.add("muro");
+
+            } else if (arrayMapa[i][j] == 3) {
+
+                newDiv.classList.add("huella");
             }
             document.querySelector(".mapa").appendChild(newDiv);
         }
     }
 
-    document.onkeydown = moverse;    
+    document.onkeydown = moverse;
 
 }
 
@@ -104,79 +104,101 @@ function reiniciarMapa() {
 
 function moverse(params) {
 
+
     switch (params.key) {
 
         case "w":
+        case "W":
+        case "ArrowUp":
+            if (posicionPersonajeY - 1 != 0) {
 
-            if (arrayMapa[posicionPersonajeY - 1] [posicionPersonajeX] != 1) {
-                reiniciarMapa();
-                moverArriba();
-                establecerMovimiento("batmanArriba");
+                if (arrayMapa[posicionPersonajeY - 1][posicionPersonajeX] != 1) {
+                    reiniciarMapa();
+                    moverArriba();
+                    establecerMovimiento("batmanArriba");
+
+                    if (arrayMapa[posicionPersonajeY][posicionPersonajeX - 1] == 1) {
+                        arrayMapa[posicionPersonajeY][posicionPersonajeX - 1] = 8;
+                        console.log("Muro a tu ?");
+
+                    }
+
+                }
             }
-
             break;
 
-       
+
 
         case "s":
-
-            if (arrayMapa[posicionPersonajeY + 1][posicionPersonajeX] != 1) {
-                reiniciarMapa();
-                moverAbajo();
-                establecerMovimiento("batmanAbajo");
+        case "S":
+        case "ArrowDown":
+            if (posicionPersonajeY + 1 != 14) {
+                if (arrayMapa[posicionPersonajeY + 1][posicionPersonajeX] != 1) {
+                    reiniciarMapa();
+                    moverAbajo();
+                    establecerMovimiento("batmanAbajo");
+                    arrayMapa[8][0] == 0;
+                }
             }
-
             break;
 
-            case "a":
+        case "a":
+        case "A":
+        case "ArrowLeft":
+            if (posicionPersonajeX - 1 != -1) {
 
-            if (arrayMapa[posicionPersonajeY][posicionPersonajeX-1] != 1) {
-                reiniciarMapa();
-                moverIzquierda();
-                establecerMovimiento("batmanIzquierda");
+
+                if (arrayMapa[posicionPersonajeY][posicionPersonajeX - 1] != 1) {
+                    reiniciarMapa();
+                    moverIzquierda();
+                    establecerMovimiento("batmanIzquierda");
+                }
             }
-
             break;
 
-            case "d":
+        case "d":
+        case "D":
+        case "ArrowRight":
+            if (posicionPersonajeX + 1 != 21) {
 
-                if (arrayMapa[posicionPersonajeY][posicionPersonajeX +1] != 1) {
+
+                if (arrayMapa[posicionPersonajeY][posicionPersonajeX + 1] != 1) {
                     reiniciarMapa();
                     moverDerecha();
                     establecerMovimiento("batmanDerecha");
                 }
-    
-                break;
-    
-    
-       
-    }
+            }
+            break;
 
+    }
+    arrayMapa[posicionPersonajeY][posicionPersonajeX] = 3;
 }
 
 function moverAbajo() {
 
     posicionPersonajeY++;
-   
+
 
 }
 
 function moverArriba() {
 
     posicionPersonajeY--;
-   
+
 
 }
+
 function moverIzquierda() {
 
     posicionPersonajeX--;
-    
-   
+
+
 
 }
+
 function moverDerecha() {
 
     posicionPersonajeX++;
-   
+
 
 }
