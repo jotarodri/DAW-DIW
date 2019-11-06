fbd73b0c00f45170157cb4ae6fbfe639ccd76f9d
Jorge Rodriguez <jorguero47@gmail.com>
2019-11-06 16:25:39 +0100

    Ejercicio 10 completed!

---
+++ UD02/Practicas/UD02_Tarea_10_TransicionesCSS/script.js
@@ -0,0 +1,2 @@
+document.querySelector("button").addEventListener("click", function moverCthulus(params) { document.querySelectorAll(".caja").forEach(element => {element.classList.toggle("transicion");})});
+
