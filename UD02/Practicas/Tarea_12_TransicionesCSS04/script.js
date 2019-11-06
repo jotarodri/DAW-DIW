7b8a958cc56877890cb8bc56f56187a7466d0186
jotarodri <44468803+jotarodri@users.noreply.github.com>
2019-11-07 00:15:23 +0100

    Tarea 12 completada!

---
+++ UD02/Practicas/Tarea_12_TransicionesCSS04/script.js
@@ -0,0 +1,39 @@
+window.onload=init;
+
+var contador = 0;
+
+function init() {
+    document.querySelector("button").addEventListener("click", crearCaja);
+}
+
+function crearCaja() {
+
+
+    const container = document.querySelector("container");
+
+    if (contador <= 19) {
+
+    let caja = document.createElement("div");
+     
+    caja.setAttribute("class","box");
+    caja.addEventListener("click", evolucionar);
+
+    container.appendChild(caja); 
+    contador++; 
+   }
+   
+}
+
+function evolucionar() {
+    this.classList.add("evoluciona");
+    this.addEventListener("click", desevolucionar);
+}
+
+function desevolucionar() {
+    this.classList.add("desevoluciona");
+    this.addEventListener("click", megaEvolucionar);
+}
+
+function megaEvolucionar() {
+    this.classList.add("ultimate");
+}
