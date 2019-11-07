17cd5821aefa29ef2dc9097180a9ad45b0dc2e08
Jorge Rodriguez <jorguero47@gmail.com>
2019-11-07 19:50:48 +0100

    SOlucionado la superposicion de las cajas

--- UD02/Practicas/Tarea_12_TransicionesCSS04/script.js
+++ UD02/Practicas/Tarea_12_TransicionesCSS04/script.js
@@ -25,15 +25,18 @@ function crearCaja() {
 }
 
 function evolucionar() {
+    this.classList.remove("ultimate");
     this.classList.add("evoluciona");
     this.addEventListener("click", desevolucionar);
 }
 
 function desevolucionar() {
+    this.classList.remove("evoluciona");
     this.classList.add("desevoluciona");
     this.addEventListener("click", megaEvolucionar);
 }
 
 function megaEvolucionar() {
+    this.classList.remove("desevoluciona");
     this.classList.add("ultimate");
 }
