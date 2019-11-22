6a18a21f378ed839b83e03b53bfa9679a858222a
Jorge Rodriguez <jorguero47@gmail.com>
2019-11-22 16:03:37 +0100

    Falta transicion de completado

--- UD03/Interfaz de Migración/migracion.js
+++ UD03/Interfaz de Migración/migracion.js
@@ -7,15 +7,38 @@ email : <berlanas_ang@gva.es>
 
 */
 
-function startMigration(){
+async function startMigration(){
 
     let dataSteps = document.querySelectorAll('[data-step]');
+    document.querySelector("button").removeEventListener("click", startMigration);
+
     for (let i = 0; i < dataSteps.length; i++) {
-        dataSteps[i].classList.add("noOculto");
-        dataSteps[i].addEventListener("transitionend", )
+
+        dataSteps[i].classList.add("estabaEscondido");
+
+        if (dataSteps[i].localName == 'progress') {
+            
+            for (let index = 0; index <= 100; index++) {
+                
+                dataSteps[i].value=index;
+
+                let promesa = await megaPromesa(10);
+                
+            }
+
+        }
         
-    }
-    //dataSteps.forEach(element => element.classList.add("noOculto"));
+        //dataSteps[i].addEventListener("transitionend", startMigration); 
+   }
+
+}
+
+function megaPromesa(x) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(x);
+        }, 50);
+    });
 }
 
 function init(){
