257f77098ebd1f08fb925e1b9e2566da7fd95180
jotarodri <44468803+jotarodri@users.noreply.github.com>
2020-01-21 01:40:54 +0100

    Graficos de barras arreglado

--- UD04/Canvas/GraficosCanvas/js/graficos.js
+++ UD04/Canvas/GraficosCanvas/js/graficos.js
@@ -1,83 +1,225 @@
-let arrayClave;
-let arrayValor;
 let dioses;
 let totalValor;
 
-let canvas;
-let ctx;
+let tipoGrafico;
 
+var myCanvas = document.querySelector("canvas");
+myCanvas.width = 500;
+myCanvas.height = 500;
 
+var ctx = myCanvas.getContext("2d");
 
+var arrayClave = document.querySelectorAll("input[class='left']");
+var arrayValor =  document.querySelectorAll("input[class='right']");
+let inputValor;
+var myVinyls;
+
+var miGraficoBarras;
 
 //let anguloActual = -0.5 * Math.PI;
 
-function buildGrafico(){
-    console.info(" * Construyendo grafico ");
-}
-
 function loadListeners(){
     document.querySelector("input[name='grafiqueame']").addEventListener("click",buildGrafico);
-    arrayClave = document.querySelectorAll("input[class='left']");
-    arrayValor = document.querySelectorAll("input[class='right']");
+    document.querySelector("select").addEventListener("click",establecerGrafico);
+     
+   document.querySelectorAll("input[class='right']")[0].addEventListener("input",function() {
+        dioses[0].poder = arrayValor[0].value;
+    })
+
+    document.querySelectorAll("input[class='right']")[1].addEventListener("input",function() {
+        dioses[1].poder = arrayValor[1].value;
+    })
+
+    document.querySelectorAll("input[class='right']")[2].addEventListener("input",function() {
+        dioses[2].poder = arrayValor[2].value;
+    })
+
+    document.querySelectorAll("input[class='right']")[3].addEventListener("input",function() {
+        dioses[3].poder = arrayValor[3].value;
+       myVinyls = [    arrayValor[0].value,
+arrayValor[1].value,
+ arrayValor[2].value,
+arrayValor[3].value
+];
+
+ miGraficoBarras = new GraficoBarra(
+    {
+        canvas:myCanvas,
+        seriesName:"Vinyl records",
+        padding:20,
+        gridScale:5,
+        gridColor:"#eeeeee",
+        data: myVinyls,
+        colors:["#a55ca5","#67b6c7", "#bccd7a","#eb9743"]
+    }
+);
+
+})
+
+  
     
-     dioses = [
-        {nombre:arrayClave[0].name, poder: arrayValor[0].name, color:"green"},
-        {nombre:arrayClave[1].name, poder: arrayValor[1].name, color:"red"},
-        {nombre:arrayClave[2].name, poder: arrayValor[2].name, color:"grey"},
-        {nombre:arrayClave[3].name, poder: arrayValor[3].name, color:"purple"}
-        ];
 }
+function buildGrafico(){
+    console.info(" * Construyendo grafico ");
+
+    
+    if (tipoGrafico=="barras") {
+    miGraficoBarras.draw();
+    }else if(tipoGrafico=="tartas"){
+
+    }else if(tipoGrafico=="puntos"){
+
+    }else{
+        borrarCanvas();
+    }
+}
+
+
+
+function establecerGrafico() {
+    tipoGrafico = this.value;
+}
+
+
+function drawLine(ctx, startX, startY, endX, endY,color){
+    ctx.save();
+    ctx.strokeStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(startX,startY);
+    ctx.lineTo(endX,endY);
+    ctx.stroke();
+    ctx.restore();
+}
+ 
+function drawBar(ctx, upperLeftCornerX, upperLeftCornerY, width, height,color){
+    ctx.save();
+    ctx.fillStyle=color;
+    ctx.fillRect(upperLeftCornerX,upperLeftCornerY,width,height);
+    ctx.restore();
+}
+
+
+
+ 
+var GraficoBarra = function(options){
+    this.options = options;
+    this.canvas = options.canvas;
+    this.ctx = this.canvas.getContext("2d");
+    this.colors = options.colors;
+  
+    this.draw = function(){
+        var maxValue = 0;
+        for (var categ in this.options.data){
+            maxValue = Math.max(maxValue,this.options.data[categ]);
+        }
+        var canvasActualHeight = this.canvas.height - this.options.padding * 2;
+        var canvasActualWidth = this.canvas.width - this.options.padding * 2;
+ 
+        //drawing the grid lines
+        var gridValue = 0;
+        while (gridValue <= maxValue){
+            var gridY = canvasActualHeight * (1 - gridValue/maxValue) + this.options.padding;
+            drawLine(
+                this.ctx,
+                0,
+                gridY,
+                this.canvas.width,
+                gridY,
+                this.options.gridColor
+            );
+             
+            //writing grid markers
+            this.ctx.save();
+            this.ctx.fillStyle = this.options.gridColor;
+            this.ctx.textBaseline="bottom"; 
+            this.ctx.font = "bold 10px Arial";
+            this.ctx.fillText(gridValue, 10,gridY - 2);
+            this.ctx.restore();
+ 
+            gridValue+=this.options.gridScale;
+        }      
+  
+        //drawing the bars
+        var barIndex = 0;
+        var numberOfBars = Object.keys(this.options.data).length;
+        var barSize = (canvasActualWidth)/numberOfBars;
+ 
+        for (categ in this.options.data){
+            var val = this.options.data[categ];
+            var barHeight = Math.round( canvasActualHeight * val/maxValue) ;
+            drawBar(
+                this.ctx,
+                this.options.padding + barIndex * barSize,
+                this.canvas.height - barHeight - this.options.padding,
+                barSize,
+                barHeight,
+                this.colors[barIndex%this.colors.length]
+            );
+ 
+            barIndex++;
+        }
+ 
+        //drawing series name
+        this.ctx.save();
+        this.ctx.textBaseline="bottom";
+        this.ctx.textAlign="center";
+        this.ctx.fillStyle = "#000000";
+        this.ctx.font = "bold 14px Arial";
+     //   this.ctx.fillText(this.options.seriesName, this.canvas.width/2,this.canvas.height);
+        this.ctx.restore();  
+        dibujaMarcoGrafica();
+       
+    }
+}
+ 
+ 
+
+
+function dibujaMarcoGrafica(ctx){
+ 
+    let x = 800;
+    let y = 480;
+
+    this.ctx.beginPath();
+    this.ctx.strokeStyle = '#000000';
+    this.ctx.lineWidth = 1;
+ 
+
+     for (let i = 0; i < 2; i++) {
+         
+        this.ctx.moveTo(0,480);
+     
+        this.ctx.lineTo(x,y);
+         x = 0;
+         y = 0;
+     }
+
+     this.ctx.stroke();
+ 
+}
+
+
+function borrarCanvas() {
+    ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+}
+
+
+
+
 
 
 function init(){
     console.log(" * Init ");
     loadListeners();
 
-    canvas = document.querySelector("canvas");
-
-// contexto -> ctx
-    ctx = canvas.getContext("2d");
-    
-    ctx.moveTo(200,200);
- 
-    dioses.reduce(function(suma,dioses) {
-        totalValor=suma =+ dioses.poder; 
-    },0);
-
-    console.log(totalValor);
-
-    graficoQuesitos();
+    dioses = [
+        {nombre:arrayClave[0].value, poder: arrayValor[0].value, color:"green"},
+        {nombre:arrayClave[1].value, poder: arrayValor[1].value, color:"red"},
+        {nombre:arrayClave[2].value, poder: arrayValor[2].value, color:"grey"},
+        {nombre:arrayClave[3].value, poder: arrayValor[3].value, color:"purple"}
+        ];
 
 }
 
-function graficoQuesitos() {
-
-    // el radio del gráfico;					
-    var r = 100;
-
-    // las coordenadas del centro del canvas
-    var X = canvas.width / 2
-    var Y = canvas.height / 2;
-
-    // dibuja un circulo gris en el centro del canvas
-    ctx.fillStyle = '#ddd';
-    ctx.moveTo(X, Y);
-    ctx.arc(X, Y, r, 0, 2 * Math.PI);
-    ctx.fill();
-
-    // dibuja un sector circular		
-    var ap = 0;
-    var af = (2 * Math.PI / 100)* totalValor ;
-    var Xap = X + r * Math.cos(ap);
-    var Yap = Y + r * Math.sin(ap);
-
-    ctx.beginPath();
-    ctx.fillStyle = "blue";
-    ctx.moveTo(X, Y);
-    ctx.lineTo(Xap, Yap);
-    ctx.arc(X, Y, r, ap, af);
-    ctx.closePath();
-    ctx.fill();
-}
 
 window.onload=init;
