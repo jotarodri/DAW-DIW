d253c19d14cf1fed7774d34c9bdaae6d65d03074
jotarodri <44468803+jotarodri@users.noreply.github.com>
2020-02-04 23:51:13 +0100

    Cambiado el metodo generico

--- IONIC/calculator.js
+++ IONIC/calculator.js
@@ -1,30 +1,47 @@
-function onLoad() {
-    document.addEventListener("ionChange", setStyle);
-    setStyle();
-}
-function setStyle() {
-    document.querySelectorAll("ion-content ion-button").forEach(function(b) {
-        b.expand = "block";
-        b.strong = "true";
-        b.fill = document.getElementById("type").value;
-        b.size = document.getElementById("size").value;
-    });
-}
-function setResult(value) {
-    document.getElementById("result").innerHTML = value;
-}
-function getResult() {
-    return(document.getElementById("result").innerHTML);
-}
-function add(key) {
-    var result = getResult();
-    if (result!="0" || isNaN(key)) setResult(result + key);
-    else setResult(key);
-}
-function calc() {
-    var result = eval(getResult());
-    setResult(result);
-}
-function del() {
-    setResult(0);
-}
+    function onLoad() {
+        document.addEventListener("ionChange", setStyle);
+        setStyle();
+
+        let botonesAdd = document.querySelectorAll("#add");
+
+        botonesAdd.forEach(boton => {
+            boton.addEventListener("click",add)
+        });
+
+        let botonDel = document.querySelector("#del");
+        botonDel.addEventListener("click",del);
+
+        let botonCalc = document.querySelector("#calc");
+        botonCalc.addEventListener("click",calc)
+
+
+    }
+    function setStyle() {
+        document.querySelectorAll("ion-content ion-button").forEach(function(b) {
+            b.expand = "block";
+            b.strong = "true";
+            b.fill = document.getElementById("type").value;
+            b.size = document.getElementById("size").value;
+        });
+    }
+    function setResult(value) {
+        document.getElementById("result").innerHTML = value;
+    }
+    function getResult() {
+        return(document.getElementById("result").innerHTML);
+    }
+    function add() {
+        let key = this.innerHTML;
+        var result = getResult();
+        if (result!="0" || isNaN(key)) setResult(result + key);
+        else setResult(key);
+    }
+    function calc() {
+        var result = eval(getResult());
+        setResult(result);
+    }
+    function del() {
+        setResult(0);
+    }
+
+
