1ba6279444afcbdf66278e81cb6408fae2849441
Jorge Rodriguez <jorguero47@gmail.com>
2020-02-12 17:37:02 +0100

    Avances en la pokedex

--- React Native/Pokemon/pokeapp/src/App.js
+++ React Native/Pokemon/pokeapp/src/App.js
@@ -6,7 +6,6 @@ import './App.css';
 
 
 
-
 class App extends Component {
   
   render() {
@@ -14,6 +13,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <Busqueda />
+        <Resultado />
     </div>
     
     );
@@ -23,15 +23,17 @@ class App extends Component {
 class Header extends Component {
   render() {
     return (
-      <div className="App">
+  
         <div className="App-header">
         <img src={letra} className="letras" alt="logo" /> 
         <img src={logo} className="App-logo" alt="logo" />
-        </div>
         <p className="App-intro">
-         
         </p>
-      </div>
+        </div>
+        
+      
+
+    
     );
   }
 }
@@ -45,6 +47,53 @@ class Busqueda extends Component {
           <input type="search" className="buscadorPokemons" placeholder="Search.."></input>
           
           <input type="button" value="Buscar" className="botonBuscar"></input>
+
+        </div>
+        
+    
+    );
+  }
+}
+
+
+class Resultado extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+     nombre: "gaston",
+     listapokemones:[]
+    };
+  }
+
+async componentDidMount(){
+ 
+let fetchPokemon = await fetch("https://pokeapi.co/api/v2/pokemon?limit=1000");
+let listaPokemon = await fetchPokemon.json();
+
+
+
+this.setState({
+  nombre: listaPokemon.results[0].name,
+  listapokemones: listaPokemon.results,
+});
+
+
+console.log(this.state.listapokemones[0].name);  
+}
+
+
+
+  render() {
+
+    return (
+
+        <div className="App-resultado">
+         
+         
+          <div className="resultado">{this.state.listapokemones[0].name}</div>
+          
+          
 
         </div>
         
